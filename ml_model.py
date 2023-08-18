a79d11af38c7178c5799df58c2d5c4e5959bc458
deepakbhadoria <deepakbhadoria@gmail.com>
2023-08-18 23:15:57 +0530

    Adding three model codes

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("Adding all ML related code in this file")
+
+def model1():
+    print("adding code for ml model 1 algo")
+
+def model2():
+    print("adding code for ml model 2 algo")
+
+def model3():
+    print("adding code for ml model 3 algo")
